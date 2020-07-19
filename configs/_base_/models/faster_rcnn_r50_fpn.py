d3dead3c660942b7335865aabf4c63a60af3d589
mobeixiaoxin <57902180+mobeixiaoxin@users.noreply.github.com>
2020-07-19 20:19:44 +0800

    Update faster_rcnn_r50_fpn.py

--- configs/_base_/models/faster_rcnn_r50_fpn.py
+++ configs/_base_/models/faster_rcnn_r50_fpn.py
@@ -1,12 +1,12 @@
 model = dict(
     type='FasterRCNN',
-    pretrained='torchvision://resnet50',
+    #pretrained='torchvision://resnet50',
     backbone=dict(
         type='ResNet',
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
+        #frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch'),
