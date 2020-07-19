819577568b17b999c135267a81f673076a0261d5
mobeixiaoxin <57902180+mobeixiaoxin@users.noreply.github.com>
2020-07-19 20:02:54 +0800

    Update fast_rcnn_r50_fpn.py

--- configs/_base_/models/fast_rcnn_r50_fpn.py
+++ configs/_base_/models/fast_rcnn_r50_fpn.py
@@ -1,13 +1,13 @@
 # model settings
 model = dict(
     type='FastRCNN',
-    pretrained='torchvision://resnet50',
+    #zxm pretrained='torchvision://resnet50',
     backbone=dict(
         type='ResNet',
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
+        #zxm frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch'),
